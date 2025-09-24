9977f8ea7f47412aca8935b50fee2fc34bc60770
kwangJong <azon2@naver.com>
2025-09-24 16:51:23 +0900

    vue router 설정완료

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('@/views/ProfileView.vue')
+    }
+
+  ],
 })
 
 export default router
